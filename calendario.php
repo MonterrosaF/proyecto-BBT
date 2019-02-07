f3444dc05a19fba90b59d9cea49138f251fa6527
Andres Monterrosa <felipe.monterrosa@hotmail.com>
2019-02-07 15:58:09 -0500

    Agregando conexion BD

--- calendario.php
+++ calendario.php
@@ -6,7 +6,13 @@
         <?php 
         try {
             require_once('includes/funciones/bd_conexion.php');
-            $sql = " SELECT * FROM eventos ";
+            $sql = " SELECT evento_id, nombre_evento, fecha_evento, hora_evento, cat_evento, icono, nombre_invitado, apellido_invitado ";
+            $sql .= " FROM eventos ";
+            $sql .= " INNER JOIN categoria_evento ";
+            $sql .= " ON eventos.id_cat_evento = categoria_evento.id_categoria ";
+            $sql .= " INNER JOIN invitados ";
+            $sql .= " ON eventos.id_inv = invitados.invitado_id ";
+            $sql .= " ORDER BY evento_id ";
             $resultado = $conn->query($sql);
         } catch (\Exception $e) {
             echo $e->getMessage();
@@ -15,12 +21,61 @@
 
         <div class="calendario">
             <?php 
-            while ($eventos = $resultado->fetch_assoc()) { ?>
-                    <pre>
-                        <?php var_dump($eventos); ?>
-                    </pre>
+            $calendario = array();
+            while ($eventos = $resultado->fetch_assoc()) {
+
+                
+                $fecha = $eventos['fecha_evento'];
+                $categoria = $eventos['cat_evento'];
+
+                $evento = array(
+                    'titulo' => $eventos['nombre_evento'],
+                    'fecha' => $eventos['fecha_evento'],
+                    'hora' => $eventos['hora_evento'],
+                    'categoria' => $eventos['cat_evento'],
+                    'icono' => 'fas' . " " . $eventos['icono'],
+                    'invitado' => $eventos['nombre_invitado'] . " " . $eventos['apellido_invitado']
+                );
+
+                $calendario[$fecha][] = $evento;
+
+                ?>
+
+
+
                 <?php 
-            } ?>
+            }  //while fetch?>
+
+                <?php 
+                    // Imprime todos los eventos
+                foreach ($calendario as $dia => $lista_eventos) { ?>
+                        <h3>
+                            <i class="fa fa-calendar"></i>
+                            <?php 
+                            setlocale(LC_TIME, 'spanish');
+                            echo strftime("%A, %d de %B del %Y", strtotime($dia)); ?>
+                        </h3>
+
+                        <?php foreach ($lista_eventos as $evento) { ?>
+                            <div class="dia">
+                                <p class="titulo"> <?php echo $evento['titulo']?> </p>
+                                <p class="hora">
+                                    <i class="fa fa-clock-o" aria-hidden="true"></i>
+                                    <?php echo $evento['fecha'] . " " . $evento['hora']?>
+                                </p>
+                                <p> 
+                                    <i class="<?php echo $evento['icono']; ?>" aria-hidden="true"></i>  
+                                    <?php echo $evento['categoria']?> 
+                                </p>
+                                <p> 
+                                    <i class="fa fa-user" aria-hidden="true"></i>
+                                    <?php echo $evento['invitado']?>
+                                 </p> 
+
+                            </div>
+                        <?php                } ?>
+                    <?php                 } ?>
+                
 
         </div>
 
