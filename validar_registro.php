0375a73c8529516e4dfecbf92ae3609ab1b8999d
Andres Monterrosa <felipe.monterrosa@hotmail.com>
2019-02-15 13:39:37 -0500

    Finalizacion BD Registro-compra

--- validar_registro.php
+++ validar_registro.php
@@ -1,46 +1,43 @@
+<?php if (isset($_POST['submit'])): 
+    $nombre = $_POST['nombre'];
+    $apellido = $_POST['apellido'];
+    $email = $_POST['email'];
+    $regalo = $_POST['regalo'];
+    $total = $_POST['total_pedido'];
+    $fecha = date('Y-m-d H:i:s');
+    $boletos = $_POST['boletos'];
+    $bonos = $_POST['bono_evento'];
+    $stickers = $_POST['sticker_evento'];
+    include_once 'includes/funciones/funciones.php';
+    $pedido = productos_json($boletos, $bonos, $stickers);
+    $eventos = $_POST['registro'];
+    $registro = eventos_json($eventos);
+    try {
+        require_once('includes/funciones/bd_conexion.php');
+        $stmt = $conn->prepare("INSERT INTO registrados (nombre_registrado, apellido_registrado, email_registrado, fecha_registro, pases_articulos, talleres_registrados, regalo, total_pagado) VALUES (?,?,?,?,?,?,?,?)");
+        $stmt->bind_param("ssssssis",$nombre,$apellido,$email,$fecha,$pedido,$registro,$regalo,$total);
+        $stmt->execute();
+        $stmt->close();
+        $conn->close();
+        header('Location: validar_registro.php?exitoso=1');
+    } catch (\Exception $e) {
+        echo $e->getMessage();
+    }
+?>
+<?php endif; ?>
 <?php include_once 'includes/templates/header.php'; ?>
 
     <section class="seccion contenedor">
-      <h2>RESUMEN REGISTRO</h2>
+      <!-- <h2>RESUMEN REGISTRO</h2> -->
 
-      <?php if (isset($_POST['submit'])): 
-        $nombre = $_POST['nombre'];
-        $apellido = $_POST['apellido'];
-        $email = $_POST['email'];
-        $regalo = $_POST['regalo'];
-        $total = $_POST['total_pedido'];
-        $fecha = date('Y-m-d H:i:s');
-
-        //  Pedidos
-         
-        $boletos = $_POST['boletos'];
-        $bonos = $_POST['bono_evento'];
-        $stickers = $_POST['sticker_evento'];
-        include_once 'includes/funciones/funciones.php';
-        $pedido = productos_json($boletos, $bonos, $stickers);
-
-        // Eventos
-
-        $eventos = $_POST['registro'];
-        $registro = eventos_json($eventos);
-
-
-        try {
-            require_once('includes/funciones/bd_conexion.php');
-            $stmt = $conn->prepare("INSERT INTO registrados (nombre_registrado, apellido_registrado, email_registrado, fecha_registro, pases_articulos, talleres_registrados, regalo, total_pagado) VALUES (?,?,?,?,?,?,?,?)");
-            $stmt->bind_param("ssssssis",$nombre,$apellido,$email,$fecha,$pedido,$registro,$regalo,$total);
-            $stmt->execute();
-            $stmt->close();
-            $conn->close();
-        } catch (\Exception $e) {
-            echo $e->getMessage();
-        }
-
-
-        ?>
-
-
-        <?php endif; ?>
+        <?php if(isset($_GET['exitoso'])):
+            if($_GET['exitoso'] == "1"):
+                echo "<h2>";
+                echo "Registro exitoso";
+                echo "</h2>";
+            endif;
+        endif; ?>
+        
     </section>
 
 <?php include_once 'includes/templates/footer.php'; ?>
